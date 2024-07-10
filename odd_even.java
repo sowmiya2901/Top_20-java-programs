67af1bdec9a9d52af5b3fc898006db1b9b1e6db6
sowmiya2901 <146068072+sowmiya2901@users.noreply.github.com>
2024-07-10 11:18:49 +0530

    Create odd_even.java

---
+++ odd_even.java
@@ -0,0 +1,10 @@
+class a{
+   public static voud main(String args[]){
+       int a=10;
+       if(a%2==0){
+        System.out.println("even number);
+       }else{
+        System.out.println("odd");
+        }
+   }
+}
