ba05e92a7715733f09e68df2629b9ae133d84c22
sowmiya2901 <146068072+sowmiya2901@users.noreply.github.com>
2024-07-11 12:59:07 +0530

    Create first_occurrence.java

---
+++ first_occurrence.java
@@ -0,0 +1,19 @@
+     package basics;
+
+public class sum_of_two_nums {
+	
+	public static void main(String[] args) {
+		String str="sowmiya";
+		char ch='o';
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)==ch) {
+				System.out.println("The character found at index:"+i);
+			}
+		}
+	}
+
+}
+
+
+output:
+The character found at index:1
